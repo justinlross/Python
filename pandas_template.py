8e28e1411868ac0b17c9744430fd01eb143a8242
Justin L Ross <37995778+justinlross@users.noreply.github.com>
2019-04-26 15:45:51 +1000

    Add files via upload

--- pandas_template.py
+++ pandas_template.py
@@ -3,37 +3,64 @@
 Created on Wed Apr  3 09:37:59 2019
 
 @author: Justin L Ross
+
+A default template for pandas to use in doing data science work.
+
+
 """
 
-# core imported data science libraries
-import pandas as pd;
-import numpy as np;
-import matplotlib as mpl;
-import matplotlib.pyplot as plt;
-import seaborn as sns;
-# %matplotlib inline; # This is just for Jupyter notebook, to display graphs inline.
+"""
+# imported libraries
+"""
 
-# extended imported data science libraries
-import csv;
-import seaborn as sns;
+# core data science libraries
+import pandas as pd; # For dealing with data, creating dataframes, importing data.
+import numpy as np; # For dealing with numbers, more efficient processing of arrays.
+import matplotlib as mpl; # For creating visualizations.
+import matplotlib.pyplot as plt; # For creating plots/graphs.
+# %matplotlib inline; # For Jupyter notebook, to display graphs inline. Uncomment if needed.
+
+# extended data science libraries
+# import csv; # Pandas includes some csv import stuff, but sometimes pythons default is better.
+import seaborn as sns; # For prettying visualizations.
 from sklearn import datasets, linear_model;
 from sklearn.metrics import mean_squared_error, r2_score;
 
 
+"""
 # importing data
+"""
+
 # df = pd.read_csv("C:\\Users\Justin\Desktop\Datasets\Heralded Diffraction SM\0001.asc", dialect=None)
 # x = np.genfromtxt("C:\\Users\Justin\Desktop\Datasets\Heralded Diffraction SM\0001.asc", dtype=None)
 # f = open("C:\\Users\Justin\Desktop\Datasets\Heralded Diffraction SM\0001.asc", 'r')
 # ds = csv.reader("C:\\Users\Justin\Desktop\Datasets\Heralded Diffraction SM\0001.asc", dialect=csv.excel_tab)
 
+"""
+# declaring constants
+"""
+
+"""
+# declaring variables
+"""
+
+
+"""
+# Other Statements Section
+"""
+# Processing the data.
+
+fig = plt.figure() 
+ax = plt.axes() 
+
+
+# Presenting the data.
+
+
 # styling
 plt.style.use('seaborn-whitegrid')
 
 
-# The Code
-
-fig = plt.figure() 
-ax = plt.axes() 
 
 
 
